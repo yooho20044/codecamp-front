181ddece415b7ba071aa59ef8a58f662870df3f6
HOJIN RYU <ryuho20044@gmail.com>
2024-05-18 15:22:10 +0900

    13-01 업로드

---
+++ pages/section13/13-01-library-icon/index.tsx
@@ -0,0 +1,21 @@
+import {UpCircleOutlined} from "@ant-design/icons"
+import styled from '@emotion/styled'
+import type { MouseEvent } from "react";
+import {useState} from 'react'
+
+const MyIcon = styled(UpCircleOutlined)`
+    color: red;
+    font-size : 50px;
+`;
+
+export default function LibraryIconPage(): JSX.Element {
+
+    const onClickDelete = (event: MouseEvent<HTMLDivElement>):void => {
+        console.log(event.currentTarget.id);
+    }
+    return (
+        <div id="삭제할게시글ID" onClick={onClickDelete}>
+            <MyIcon />
+        </div>
+    )
+}
